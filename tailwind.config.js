4d89aaf107624fbdb8219f263b8fb7638737f10d
nasimrifat101 <nasimrifat101@gmail.com>
2024-07-09 12:25:50 +0600

    configured tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#DEF9C4',
+        secondary: '#9CDBA6',
+        'text-primary': '#1A2130',
+        'text-secondary': '#D6EFD8',
+      },
+      keyframes: {
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' }
+        }
+      },
+      animation: {
+        'spin-slow': 'spin 5s linear infinite'
+      }
+    },
   },
   plugins: [],
 }
