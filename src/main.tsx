c7513e97fa8856eb74abd0786ad009c42ceb25d6
nasimrifat101 <nasimrifat101@gmail.com>
2024-07-09 13:13:23 +0600

    [fixed] rpoting

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import { router } from './routes/router';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element:<App/>,
-  },
-]);
+
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
